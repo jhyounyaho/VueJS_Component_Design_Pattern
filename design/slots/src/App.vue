b5b51916f5c581c2650e35cf6287aeef5f4c71d3
jhyounyaho <jhyounyaho@gmail.com>
2021-01-13 00:04:57 +0900

    컴포넌트 디자인 패턴 - Slot

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <ul>
-      <item>아이템 1</item>
-      <item>아이템 2</item>
-      <item>아이템 3</item>
-      <item>아이템 4</item>
+      <item>
+        아이템 1
+      </item>
+      <item>
+        아이템 2
+        <button>click me</button>
+      </item>
+      <item>
+        <div>
+          아이템 3
+        </div>
+        <img src="./assets/endgame.png" alt="어벤져스 엔드게임" width="100px">
+      </item>
+      <item>
+        <div style="color: blue; font-size: 20px;">
+          아이템4
+        </div>
+      </item>
       <item>아이템 5</item>
+      <!--item v-for="item in items" :propsdata="item" :key="item">{{ item }}</item-->
     </ul>
   </div>
 </template>
@@ -16,6 +31,11 @@ import Item from './Item.vue';
 export default {
   components: {
     Item,
+  },
+  data() {
+    return {
+      items: ['아이템1', '아이템2', '아이템3', '아이템4', '아이템5'],
+    }
   },
 }
 </script>
--- design/slots/src/Item.vue
+++ design/slots/src/Item.vue
@@ -3,5 +3,12 @@
     <slot>
       <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
     </slot>
+    <!--{{ propsdata }}-->
   </li>
 </template>
+
+<script>
+export default {
+  //props: ['propsdata'],
+}
+</script>
